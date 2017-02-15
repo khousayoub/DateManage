c3444c82d004433322bd668c301c55c0ef78d54a
ayb <khous.ayoub@gmail.com>
2017-02-15 23:17:53 +0100

    add weekend case v1.0

--- EquiTable.php
+++ EquiTable.php
@@ -1,20 +1,37 @@
 <?php
 function equiTable($begDate,$endDate,$nbDate,$weekEnd) {
 
-    // get the date format as YYYY-MM-DD
+    // get the name of both start and end dates
     $begDay = substr($begDate,0,-11);
     $endDay = substr($endDate,0,-11);
 
     // Convert the date format into timestamp to be able to calculate the interval between available dates
     $begDate = new dateTime(substr($begDate,-10));
     $endDate = new dateTime(substr($endDate,-10));
-    if ($weekEnd){
-      echo "in work !";
+    if (!$weekEnd){ // when we have to return dates without weekends
+
+      // get the timestamp of boot start and end dates into tmp variables
+      $tmpBeg = $begDate->getTimestamp();
+      $tmpEnd = $endDate->getTimestamp();
+      // Calculating the number of dates between the start date and the end date without weekends
+      $cpt = 0;
+      while ($tmpBeg <= $tmpEnd) {
+        $dateTmp = new dateTime();
+        if ( ($dateTmp->setTimestamp($tmpBeg)->format("l") !== "Saturday") &&
+         ($dateTmp->setTimestamp($tmpBeg)->format("l") !== "Sunday") )
+           $cpt ++;
+        $tmpBeg += 86400;
+      }
+      echo $cpt;
     }
-    else {
-      // Calculating the interval between available dates as timestamp
+    else { // when we have to return dates with weekends
+
+      // Calculating the number of dates between the start date and the end date (weekends include)
       $diff = (($endDate->getTimestamp() - $begDate->getTimestamp())+(3600*24))/(3600*24);
+
+      // when the numbers of days we must return is less than the numbers of available days
       if ($diff > $nbDate) {
+
       // initializing the first date to start date
       $firstDate = $begDate->getTimestamp();
 
@@ -31,13 +48,15 @@ function equiTable($begDate,$endDate,$nbDate,$weekEnd) {
 
       // show them
       print_r($tab);
-
-      echo $result."<br>";
       echo "Number of days : ".$diff;
-      } else {
+
+    }
+
+     // when the numbers of days we must return is more than the numbers of available days
+      else {
       echo "do another stuff !";
         }
   }
 }
-equiTable("Wednesday 2017-01-01","Thursday 2017-01-02", 10 , false);
+equiTable("2017-01-01","2017-01-10", 10 , false);
 ?>
