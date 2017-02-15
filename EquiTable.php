23753c3629eb0d0c1a2838061f311c0e12ccc6eb
ayb <khous.ayoub@gmail.com>
2017-02-15 22:14:53 +0100

    date repo

---
+++ EquiTable.php
@@ -0,0 +1,28 @@
+<?php
+function equiTable($begDate,$endDate,$nbDate,$weekEnd) {
+    $begDay = substr($begDate,0,-11);
+    $endDay = substr($endDate,0,-11);
+
+    $begDate = new dateTime(substr($begDate,-10));
+    $endDate = new dateTime(substr($endDate,-10));
+
+    $diff = ($endDate->getTimestamp() - $begDate->getTimestamp())/(3600*24);
+
+    $firstDate = $begDate->getTimestamp();
+
+    $intervalDate = floor($diff / $nbDate)*3600*24;
+
+    for ($i = 0; $i < $nbDate ; $i++) {
+      $firstDate += $intervalDate;
+      $date1 = new dateTime();
+      $tab[$i] = $date1->setTimestamp($firstDate)->format("Y-m-d l");
+      unset($date1);
+    }
+    print_r($tab);
+
+    echo $result."<br>";
+    echo ($endDate->getTimestamp() - $begDate->getTimestamp())/(3600*24);
+
+}
+equiTable("wednsday 2017-01-01","thursday 2017-01-31", 3 , true);
+?>
