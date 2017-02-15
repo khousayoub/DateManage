39786f0bff4c4df112380c961ffe20cd22a036fb
ayb <khous.ayoub@gmail.com>
2017-02-15 22:23:59 +0100

    add comments

--- EquiTable.php
+++ EquiTable.php
@@ -1,23 +1,32 @@
 <?php
 function equiTable($begDate,$endDate,$nbDate,$weekEnd) {
+
+    // get the date format as YYYY-MM-DD
     $begDay = substr($begDate,0,-11);
     $endDay = substr($endDate,0,-11);
 
+    // Convert the date format into timestamp to be able to calculate the interval between available dates
     $begDate = new dateTime(substr($begDate,-10));
     $endDate = new dateTime(substr($endDate,-10));
 
+    // Calculating the interval between available dates as timestamp
     $diff = ($endDate->getTimestamp() - $begDate->getTimestamp())/(3600*24);
 
+    // initializing the first date to start date
     $firstDate = $begDate->getTimestamp();
 
+    // Calculating the interval as number of dates
     $intervalDate = floor($diff / $nbDate)*3600*24;
 
+    // save dates into an Array
     for ($i = 0; $i < $nbDate ; $i++) {
       $firstDate += $intervalDate;
       $date1 = new dateTime();
       $tab[$i] = $date1->setTimestamp($firstDate)->format("l Y-m-d");
       unset($date1);
     }
+
+    // show them
     print_r($tab);
 
     echo $result."<br>";
